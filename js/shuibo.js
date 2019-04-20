6f35cc99c8b3237cbd116a02a5ab4e82e29d5edb
zhucj <1107747178@qq.com>
2019-04-20 21:22:04 +0800

    Site updated: 2019-04-20 21:22:01

--- js/shuibo.js
+++ js/shuibo.js
@@ -1,9 +1,18 @@
 (function($){
+  var middlecontent = document.getElementById("middlecontent");
+  middlecontent.style.height = document.body.scrollHeight+"px";
+alert(ismobile());
+  if(ismobile()){
+    resize();
+  }
 
-var middlecontent = document.getElementById("middlecontent");
-middlecontent.style.height = document.body.scrollHeight+"px";
+
 
 window.onresize = function(){
+  resize();
+}
+
+function resize(){
   var bodyWidth = middlecontent.offsetWidth;
   if(bodyWidth<=805){
   //document.getElementById("widget-box-left").style.position ='static';
@@ -16,4 +25,27 @@ window.onresize = function(){
     $("#main").css("width","100%");
   }
 }
+//判断用什么访问网页
+function ismobile() {
+  //平台、设备和操作系统
+      var system = {
+          win: false,
+          mac: false,
+          xll: false,
+          ipad:false
+      };
+      //检测平台
+      var p = navigator.platform;
+      system.win = p.indexOf("Win") == 0;
+      system.mac = p.indexOf("Mac") == 0;
+      system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+      system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+      //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
+      if (system.win || system.mac || system.xll||system.ipad) {
+          return false;
+      } else {
+
+          return true;//移动端
+      }
+ }
 })(jQuery);
